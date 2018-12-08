5165dc1228ff13bed85c5f19b33eecbb63584ac9
izan22 <izan_espinosa_93@hotmail.com>
2018-12-08 17:04:14 +0100

    PET-CLINIC added pettypes to DataLoader

--- pet-clinic-web/src/main/java/izan/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/izan/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,11 +1,11 @@
 package izan.springframework.petclinic.bootstrap;
 
 import izan.springframework.petclinic.model.Owner;
+import izan.springframework.petclinic.model.PetType;
 import izan.springframework.petclinic.model.Vet;
 import izan.springframework.petclinic.services.OwnerService;
+import izan.springframework.petclinic.services.PetTypeService;
 import izan.springframework.petclinic.services.VetService;
-import izan.springframework.petclinic.services.map.OwnerMapService;
-import izan.springframework.petclinic.services.map.VetMapService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michel");
